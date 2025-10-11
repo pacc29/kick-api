7aae805aaa2190691244571bc198a7be5fee917a
Piero <pierocarneiro2908@gmail.com>
2025-10-10 23:26:34 -0500

    Changed type of state column on baseentity

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,16 +1,21 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
-import { state } from "../enums/state.enum";
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { state } from '../enums/state.enum';
 
 export abstract class BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'tinyint', default: state.STATE_ENABLED })
-    state: state;
+  @Column({ type: 'boolean', default: state.STATE_ENABLED })
+  state: state;
 
-    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
-    created_at: Date;
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
 
-    @Column({ type: 'datetime', onUpdate: 'CURRENT_TIMESTAMP', nullable: true, default: null })
-    updated_at: Date;
+  @Column({
+    type: 'datetime',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    nullable: true,
+    default: null,
+  })
+  updated_at: Date;
 }
